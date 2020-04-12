48a7f8ebb7641ae4391275ecd37e44a66ab453a9
Albel Singh Bhodeliya <albelsingh31@gmail.com>
2020-04-12 11:43:06 +0530

    Event Handling

--- SpringDemo/src/main/java/com/SpringDemo/Circle.java
+++ SpringDemo/src/main/java/com/SpringDemo/Circle.java
@@ -5,13 +5,17 @@ import javax.annotation.Resource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Required;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.ApplicationEventPublisherAware;
 import org.springframework.context.MessageSource;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
-
-public class Circle implements Shape{
+@Component
+public class Circle implements Shape,ApplicationEventPublisherAware{
      
 	private Point center;
+	private ApplicationEventPublisher publisher;
 	@Autowired
 	private MessageSource messageSource;
 	
@@ -26,9 +30,9 @@ public class Circle implements Shape{
 	public void draw() {
 		System.out.println("Drawing Circle ");
 		System.out.println("Point are = "+center.getX()+","+center.getY());
-		/*System.out.println(this.messageSource.getMessage("drawing.point", new Object[] {center.getX(), center.getY()} , "Default Point ", null));
-		System.out.println(this.messageSource.getMessage("greeting", null, "Default Hello Albel", null));
-		*/
+		DrawEvent drawevent=new DrawEvent(this);
+		publisher.publishEvent(drawevent);
+		
 	}
 
 	public Point getCenter() {
@@ -39,6 +43,11 @@ public class Circle implements Shape{
 	/*@Resource(name="pointC")*/
 	public void setCenter(Point center) {
 		this.center = center;
+	}
+
+	public void setApplicationEventPublisher(ApplicationEventPublisher publisher) {
+		this.publisher=publisher;
+		
 	}
 	
 	
